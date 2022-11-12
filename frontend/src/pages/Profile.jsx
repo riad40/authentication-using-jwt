05224544c44a4b9c6c47c78558bc41c9422b49b4
riad40 <riadabdelaziz40@gmail.com>
2022-11-12 12:36:12 +0100

    make log out functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,34 +1,48 @@
-import { Link } from 'react-router-dom'
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import AuthContext from '../context/AuthContext'
 import { api } from '../helpers/api'
 import FormContainer from '../components/FormContainer'
+import { useNavigate } from 'react-router-dom'
 
 function Profile() {
 
   const [me, setMe] = useState('')
   const [err, setErr ] = useState('')
+  const Navigate = useNavigate()
 
   const { isAuth } = useContext(AuthContext)
 
   const role = isAuth.user.role || 'customer'
 
-  api.get(`/user/${role}/me`, {
-    headers: {'set-cookie': document.cookie},
-    withCredentials: true
-  }).then((response) => {
-      setMe(response.data.message)
-    })
-    .catch((err) => {
-      setErr(err.response.data.message)
-    })
+  const logout = () => {
+    api.get(`/auth/logout`, {
+      withCredentials: true
+    }).then((response) => {
+        Navigate('/')
+      })
+      .catch((err) => {
+        setErr(err.response.data.message)
+      })
+  }
+
+  useEffect(() => {
+    api.get(`/user/${role}/me`, {
+      headers: {'set-cookie': document.cookie},
+      withCredentials: true
+    }).then((response) => {
+        setMe(response.data.message)
+      })
+      .catch((err) => {
+        setErr(err.response.data.message)
+      })
+  })
     
   return (
     <FormContainer>
       <div className="text-white text-center">
         <h1>{ !err && me }</h1>
         <p className='text-red-200'>{ err }</p>
-        <p className='text-color'>Log Out</p>
+        <button className='text-color' onClick={ logout }>Log Out</button>
       </div>
     </FormContainer>
   )
