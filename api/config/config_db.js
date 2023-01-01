3a7744de7d0dd4dffa465c6523726cbe16093bed
riad40 <riadabdelaziz40@gmail.com>
2023-01-01 15:49:58 +0100

    siwtch from local db to cloud db

--- api/config/config_db.js
+++ api/config/config_db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-// const dbUri = process.env.DB_URI
+const dbUri = process.env.DB_URI
 const dbUriLocal = process.env.DB_URI_LOCAL
 
-mongoose.connect(dbUriLocal)
-    .then(() => { console.log('connected succefully to marhaba') })
+mongoose.connect(dbUri)
+    .then(() => { console.log('connected succefully to marhaba db') })
     .catch((err) => { console.log('something went wrong ' + err) })
